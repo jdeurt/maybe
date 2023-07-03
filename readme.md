b9b1519a1dad60c6ba6ab821db6d79906aa37ea3
Juan de Urtubey <jdeurt@gmail.com>
2023-07-03 11:24:06 -0500

    chore: docs

--- readme.md
+++ readme.md
@@ -1,5 +1,85 @@
-# ...
+# @jdeurt/maybe
 
-## License
+An implementation of the Maybe monad in Typescript.
 
-MIT © [Juan de Urtubey](https://jdeurt.xyz)
+## Installation
+
+To install, run:
+
+```bash
+yarn add @jdeurt/maybe
+```
+
+## API Documentation
+
+### Class: `Maybe<A>`
+
+`Maybe<A>` is an abstract class that represents a type that may either hold a value of type `A` (represented as `Just<A>`) or not (represented as `Nothing<A>`).
+
+#### Static Method: `of`
+
+Creates a new `Maybe<A>` instance.
+
+#### Static Method: `nothing`
+
+Creates a new `Nothing<A>` instance.
+
+#### Static Method: `just`
+
+Creates a new `Just<A>` instance.
+
+#### Method: `isNothing`
+
+Returns `true` if the `Maybe<A>` instance is `Nothing<A>`, `false` otherwise.
+
+#### Method: `isJust`
+
+Returns `true` if the `Maybe<A>` instance is `Just<A>`, `false` otherwise.
+
+#### Method: `map`
+
+Transforms the value in the `Just<A>` using the function, if the `Maybe<A>` is `Just<A>`. If the `Maybe<A>` is `Nothing<A>`, does nothing.
+
+#### Method: `flatMap`
+
+Similar to `map`, but the provided function should return a `Maybe<B>` instance.
+
+#### Method: `apply`
+
+Applies the function contained in the `Maybe<A>` to another `Maybe<A>`, if both `Maybe<A>` instances are `Just<A>`. If either `Maybe<A>` is `Nothing<A>`, does nothing.
+
+#### Method: `equals`
+
+Checks if two `Maybe<A>` instances are equal.
+
+#### Method: `getOrElse`
+
+If the `Maybe<A>` instance is `Just<A>`, returns its value, otherwise returns the default value.
+
+#### Method: `orElse`
+
+If the `Maybe<A>` instance is `Just<A>`, returns it, otherwise returns `Just<A>` of the default value.
+
+#### Method: `get`
+
+Returns the value of the `Maybe<A>` instance if it's `Just<A>`, otherwise throws an error.
+
+#### Method: `toString`
+
+Returns the string representation of the `Maybe<A>` instance.
+
+### Class: `Nothing<A>`
+
+`Nothing<A>` is a class that represents the absence of a value. It extends `Maybe<A>`.
+
+#### Static Method: `of`
+
+Creates a new `Nothing<A>` instance.
+
+### Class: `Just<A>`
+
+`Just<A>` is a class that represents the presence of a value. It extends `Maybe<A>`.
+
+#### Static Method: `of`
+
+Creates a new `Just<A>` instance.
--- src/maybe.ts
+++ src/maybe.ts
@@ -1,60 +1,120 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
+/**
+ * Maybe monad implementation for handling optional values and null safety.
+ */
 export abstract class Maybe<A> {
     constructor(private readonly value: A) {}
 
+    /**
+     * Creates a new Maybe instance.
+     * @param value The value to be wrapped.
+     * @returns A Just(value) if value is not null or undefined, otherwise returns Nothing.
+     */
     static of<A>(value: A): Maybe<A> {
         return value === null || value === undefined
             ? Nothing.of()
             : Just.of(value);
     }
 
+    /**
+     * Creates a new Nothing instance.
+     * @returns A Nothing instance.
+     */
     static nothing<A>(): Maybe<A> {
         return Nothing.of();
     }
 
+    /**
+     * Creates a new Just instance.
+     * @param value The value to be wrapped.
+     * @returns A Just instance with the value.
+     */
     static just<A>(value: A): Maybe<A> {
         return Just.of(value);
     }
 
+    /**
+     * Checks if the Maybe instance is Nothing.
+     * @returns True if the Maybe instance is Nothing, false otherwise.
+     */
     isNothing(): boolean {
         return this.value === null || this.value === undefined;
     }
 
+    /**
+     * Checks if the Maybe instance is Just.
+     * @returns True if the Maybe instance is Just, false otherwise.
+     */
     isJust(): boolean {
         return !this.isNothing();
     }
 
+    /**
+     * Transforms the value in the Just using the function f, if the Maybe is Just.
+     * If the Maybe is Nothing, does nothing.
+     * @param f The function to transform the value.
+     * @returns A new Maybe instance with the transformed value.
+     */
     map<B>(f: (a: A) => B): Maybe<B> {
-        return this.isNothing()
-            ? Maybe.of(undefined as never)
-            : Maybe.of(f(this.value));
+        return this.isNothing() ? Maybe.nothing() : Maybe.of(f(this.value));
     }
 
+    /**
+     * Transforms the value in the Just using the function f, if the Maybe is Just.
+     * If the Maybe is Nothing, does nothing.
+     * The function f should return a Maybe instance.
+     * @param f The function to transform the value.
+     * @returns A new Maybe instance with the transformed value.
+     */
     flatMap<B>(f: (a: A) => Maybe<B>): Maybe<B> {
-        return this.isNothing() ? Maybe.of(undefined as never) : f(this.value);
+        return this.isNothing() ? Maybe.nothing() : f(this.value);
     }
 
+    /**
+     * Applies the function contained in the Maybe to another Maybe, if both Maybe instances are Just.
+     * If either Maybe is Nothing, does nothing.
+     * @param fab The Maybe instance containing the function to apply.
+     * @returns A new Maybe instance with the result of applying the function.
+     */
     apply<B>(fab: Maybe<(a: A) => B>): Maybe<B> {
         return this.isNothing()
-            ? Maybe.of(undefined as never)
+            ? Maybe.nothing()
             : fab.map((f) => f(this.value));
     }
 
+    /**
+     * Checks if two Maybe instances are equal.
+     * @param ma The other Maybe instance to compare with.
+     * @returns True if the two Maybe instances are equal, false otherwise.
+     */
     equals(ma: Maybe<A>): boolean {
         return this.isNothing()
             ? ma.isNothing()
             : ma.isJust() && this.value === ma.value;
     }
 
-    getOrElse(a: A): A {
-        return this.isNothing() ? a : this.value;
+    /**
+     * If the Maybe instance is Just, returns its value, otherwise returns the default value.
+     * @param defaultValue The default value to return if the Maybe instance is Nothing.
+     * @returns The value of the Maybe instance if it's Just, otherwise the default value.
+     */
+    getOrElse(defaultValue: A): A {
+        return this.isNothing() ? defaultValue : this.value;
     }
 
-    orElse(a: A): Maybe<A> {
-        return this.isNothing() ? Maybe.of(a) : this;
+    /**
+     * If the Maybe instance is Just, returns it, otherwise returns Just(defaultValue).
+     * @param defaultValue The default value to wrap in a Just if the Maybe instance is Nothing.
+     * @returns The original Maybe instance if it's Just, otherwise Just(defaultValue).
+     */
+    orElse(defaultValue: A): Maybe<A> {
+        return this.isNothing() ? Maybe.of(defaultValue) : this;
     }
 
+    /**
+     * Returns the value of the Maybe instance if it's Just, otherwise throws an error.
+     * @throws {Error} Throws an error if the Maybe instance is Nothing.
+     * @returns The value of the Maybe instance.
+     */
     get(): A {
         if (this.isNothing()) {
             throw new Error("Cannot get value from Nothing");
@@ -63,23 +123,42 @@ export abstract class Maybe<A> {
         return this.value;
     }
 
+    /**
+     * Returns the string representation of the Maybe instance.
+     * @returns A string representing the Maybe instance.
+     */
     toString(): string {
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         return this.isNothing() ? "Nothing" : `Just(${this.value})`;
     }
 }
 
+/**
+ * Nothing class for representing the absence of a value.
+ */
 export class Nothing<A> extends Maybe<A> {
     constructor() {
         super(undefined as never);
     }
 
+    /**
+     * Creates a new Nothing instance.
+     * @returns A Nothing instance.
+     */
     static of<A>(): Maybe<A> {
         return new Nothing();
     }
 }
 
+/**
+ * Just class for representing the presence of a value.
+ */
 export class Just<A> extends Maybe<A> {
+    /**
+     * Creates a new Just instance.
+     * @param value The value to be wrapped.
+     * @returns A Just instance with the value.
+     */
     static of<A>(value: A): Maybe<A> {
         return new Just(value);
     }
